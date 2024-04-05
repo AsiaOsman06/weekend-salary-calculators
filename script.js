5789a5db1c6c8cbc3b12f4823e6b8b140712879f
Asia Osman <asiaosman06@gmail.com>
2024-04-04 23:03:47 -0500

    added currencty formatted string in the annual salary row

--- script.js
+++ script.js
@@ -1,50 +1,80 @@
-let monthlyPayment = 0;
+// Global variable to store the current total annual salary:
+let totalAnnualSalary = 0;
+
+//This function performs all actions necessary when a user clicks the form's "Submit" button:
 function addEmployee(event) {
   event.preventDefault();
 
+  // Get the input values:
   let firstNameText = document.getElementById("firstNameText").value;
   let lastNameInput = document.getElementById("lastNameText").value;
   let idInput = document.getElementById("idText").value;
   let titleInput = document.getElementById("titleText").value;
-  let annualSalaryInput = document.getElementById("annualSalaryText").value;
-  let employeesData = document.getElementById("employeesDetails");
-  let monthlySalary = Number(annualSalaryInput /12);
-  employeesData.innerHTML += `
+  let annualSalaryInput = Number(
+    document.getElementById("annualSalaryText").value
+  );
+  let newEmployeeRow = document.getElementById("employeesDetails");
+
+  // Make the new employee row:
+  newEmployeeRow.innerHTML += `
 
             <tr>
              <td id="employeesDetails">${firstNameText}</td>
              <td id="employeesDetails">${lastNameInput}</td>
              <td id="employeesDetails">${idInput}</td>
              <td id="employeesDetails">${titleInput}</td>
-             <td id="employeesDetails">${annualSalaryInput}</td>
-             <td ><button onclick="toDelete(event)">␡</button></td>
+             <td id="employeesDetails">${formatAsUSD(annualSalaryInput)}</td>
+             <td>
+             <button onclick="toDelete(event)">␡</button>
+             </td>
             </tr>
   `;
-  //set back to an empty string
+  // clear put the form inputs:
   document.getElementById("firstNameText").value = "";
   document.getElementById("lastNameText").value = "";
   document.getElementById("idText").value = "";
   document.getElementById("titleText").value = "";
   document.getElementById("annualSalaryText").value = "";
-  updateTotalMonthlyCost(monthlySalary);
+
+  // Update the current total annual Salary:
+  totalAnnualSalary += Number(annualSalaryInput);
+
+  // Calculate the Total Monthly Salary:
+  totalMonthlySalary = totalAnnualSalary / 12;
+
+  // Update the DOM to display the new total monthy salary:
+  let updatedMonth = document.getElementById("totalMonthlySalary");
+  updatedMonth.textContent = formatAsUSD(totalMonthlySalary);
+
+  // Conditionally apply the 'over-budget' CSS class to the footer if total monthly salary is above 20,000:
+
+  if (totalMonthlySalary > 20000) {
+    let footer = document.getElementById("myFooter");
+    footer.classList.add("over-budget");
+  }
 }
 
-function updateTotalMonthlyCost(monthlyPayment) {
-  let updatedMonth = document.getElementById("totalMonthlyCost").value;
-  let footer = document.getElementById("footer").value;
-  monthlyPayment++;
-  const currentTotal = parseFloat(updatedMonth.textContent);
-  const newTotal = currentTotal + monthlySalary;
-  updatedMonth.textContent = newTotal.toFixed(2);
-  if (newTotal > 20000) {
-    footer.classList.add("over-budget");
-  } else {
-    footer.classList.remove("over-budget");
-  }
-document.getElementById("footer").value='';
-document.getElementById("totalMonthlyCost").value='';
-}
+//This function performs all actions necessary when a user clicks an employee row's "Delete" button:
 function toDelete(event) {
-  let parent = event.target.parentElement.parentElement;
-  parent.remove();
+  // let parent = event.target.parentElement.parentElement;
+  // Select the exact button that was clicked:
+  let clickedButton = event.target;
+  // Select the table row that the clicked button is inside of:
+  let employeeRowToDelete = clickedButton.closest("tr");
+  // Delete the table row from the DOM:
+  employeeRowToDelete.remove();
+}
+// Takes in a number and returns a currencty-formatted string:
+// Docs here:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
+// Usage examples:
+  // formatAsUSD(50500.333333)
+      // '$50,500.33'
+  // formatAsUSD(22)
+      // '$22.00'
+function formatAsUSD(number) {
+  return new Intl.NumberFormat(
+    'en-EN',
+    { style: 'currency', currency: 'USD' }
+  ).format(number);
 }
